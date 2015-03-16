6c403a8ba79d8c4d417ad34932f9e6451ae3f786
bli15 <bli15@as31112.punahou.edu>
2015-03-16 13:21:20 -1000

    First commit
    Currently have the board display working, chip dropper working. Need to
    code win check then I will move onto frame

---
+++ fourDriver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class fourDriver{
+  
+  public static void main (String[] args){
+    
+    Scanner input = new Scanner(System.in);
+    
+    fourGame game = new fourGame();
+    
+    String reply;
+    System.out.println("Would you like to create a new game? (Yes or No)");
+    reply = input.next();
+    if (reply.equals("yes") || reply.equals("Yes")){
+      game.createGame();
+    }
+    
+    if (reply.equals("no") || reply.equals("No")){
+      System.out.println("Okay, just say when you want to play");
+    }
+    while (game.win() == false)
+    game.game();
+  }
+}
